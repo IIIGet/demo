b100b39596a83dd5c18db6dab58071799ecb7d53
ingsgreat <1209455541@qq.com>
2021-07-26 20:18:53 +0800

    更新了order.html的样式

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -3,7 +3,6 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.rmi.registry.Registry;
 
 
 @SpringBootApplication()
